33fe21a11ac98cd1c7375dc162c56dd41d22127c
Andreisr <pidecoribeiro2007@gmail.com>
2024-09-20 12:43:40 -0300

    coloquei a função de registro

--- projeto/logins.py
+++ projeto/logins.py
@@ -1,6 +1,17 @@
 from tkinter import messagebox
 import tkinter as tk
-import back
+import pymysql
+
+conexao = pymysql.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    database = 'high'
+)
+
+nome = ''
+email = ''
+senha = ''
 
 def retornar(janela_atual):
     
@@ -13,14 +24,59 @@ def sair(janela_atual):
     r = messagebox.askquestion('Fechar a janela', 'Você realmente deseja fechar a janela?')
     if r.lower() == 'yes':
         janela_atual.destroy()
+
+def registro():
+    global n_e, e_e, s_e, nome, email, senha
+    
+    nome = n_e.get()
+    email = e_e.get()
+    senha = s_e.get()
+    
+    try:
+        with conexao.cursor() as cursor:
+            sql = "INSERT INTO registro(nome, email, senha) VALUES(%s, %s, %s)"
+            cursor.execute(sql, (nome, email, senha))
+            s = cursor.fetchall()
+            conexao.commit()
+            if s == None:
+                messagebox.showinfo('Usuario negado', 'Acesso negado, repita as informações!')
+            else:
+                messagebox.showinfo('Usuario aceito', 'Usuário registrado com sucesso, agora façã login!')
+                
+    except pymysql.Error as error:
+        conexao.rollback()
+        messagebox.showerror('Erro', f'{error}')
         
+            
 def pergunta():
     global janela3
     
     janela4 = tk.Tk()
     janela4.title('Pergunta e resposta')
     
+    t = tk.Label(janela4, text='Perguntas', font=('Georgia', 20))
+    t.place(rely=0.1, relx=0.38)
     
+    t1 = tk.Label(janela4, text='E', font=('Georgia', 18))
+    t1.place(rely=0.135, relx=0.45)
+    
+    t2 = tk.Label(janela4, text='Respostas', font=('georgia', 20))
+    t2.place(rely=0.165, relx=0.465)
+    
+    b_c = tk.Button(janela4, text='Criar Perguntas', height=2, width =15, border =3, borderwidth=3,)
+    b_c.place(rely=0.45, relx=0.35)
+    
+    b_r = tk.Button(janela4, text='Respostas', height=2, width=15, border=3, borderwidth=3,)
+    b_r.place(rely=0.45, relx=0.45)
+    
+    b_p = tk.Button(janela4, text='Perguntas', height=2, width=15, border=3, borderwidth=3, )
+    b_p.place(rely =0.45, relx=0.55)
+    
+    b_r = tk.Button(janela4, text='Retornar', height=2, width=17, border=3, borderwidth=3, command=lambda: (retornar(janela2)))
+    b_r.place(rely=0.94, relx=0.02)
+    
+    b_s = tk.Button(janela4, text='Sair', height=2, width=17, border=3, borderwidth=3, command=lambda: (sair(janela)))
+    b_s.place(rely=0.94, relx=0.9)
     
     janela4.mainloop()
         
@@ -60,7 +116,7 @@ def login():
     janela3.mainloop()
 
 def registrar():
-    global janela
+    global janela, n_e, e_e, s_e
     
     janela.destroy()
     
@@ -88,7 +144,7 @@ def registrar():
     s_e = tk.Entry(janela2, font=20 ,border=3, borderwidth=3, show='*')
     s_e.place(rely=0.405, relx=0.47)
     
-    b = tk.Button(janela2, text='Registrar', height=1, width=8, border=3, borderwidth=3, )
+    b = tk.Button(janela2, text='Registrar', height=1, width=8, border=3, borderwidth=3, command=registro)
     b.place(rely=0.45, relx=0.485)
     
     b_r = tk.Button(janela2, text='Retornar', height=2, width=17, border=3, borderwidth=3, command=lambda: (retornar(janela2)))
@@ -122,4 +178,4 @@ def menu():
 
     janela.mainloop()
     
-pergunta()
+menu()
