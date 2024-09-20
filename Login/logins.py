dfa7f9c12c3d04098b16edc8500f14578fc21cc4
Andreisr <pidecoribeiro2007@gmail.com>
2024-09-20 06:32:17 -0300

    menu feito

--- Login/logins.py
+++ Login/logins.py
@@ -10,7 +10,26 @@ conexao = pymysql.connect(
 
 cursor = conexao.cursor()
 
-janela = tk.Tk()
-janela.title('menu')
+def menu():
+    janela = tk.Tk()
+    janela.title('menu')
 
-janela.mainloop()
+    t = tk.Label(janela, text='Menu', font=('Arial', 20))
+    t.place(rely=0.15, relx=0.46)
+    
+    b_r = tk.Button(janela, text='Registro', height=2, width =15, border =3, borderwidth=3, )
+    b_r.place(rely=0.45, relx=0.35)
+    
+    b_l = tk.Button(janela, text='Login', height=2, width=15, border=3, borderwidth=3, )
+    b_l.place(rely=0.45, relx=0.45)
+    
+    b_i = tk.Button(janela, text='Informações', height=2, width=15, border=3, borderwidth=3, )
+    b_i.place(rely =0.45, relx=0.55)
+    
+    b_s = tk.Button(janela, text='Sair', height=2, width=17, border=3, borderwidth=3, )
+    b_s.place(rely=0.94, relx=0.9)
+
+    janela.mainloop()
+
+    
+menu()
